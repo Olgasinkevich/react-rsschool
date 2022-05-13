109e785b63294082f59a7ecf0a3abf5aa5a9a67a
Olgasinkevich <sinkevichweb@gmail.com>
2022-05-13 12:12:08 +0300

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-https://Olgasinkevich.github.io/react-rsschool/
-https://Olgasinkevich.github.io/react-rsschool/public
-
--- webpack.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    entry: './src/index.tsx',
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-};
